f538ef140bc5f387cae14e7b6fa071bb736f2baf
Andy <andy64lolxd@gmail.com>
2025-03-28 17:37:03 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const iframe = document.getElementById('browser-frame');
+const urlBar = document.getElementById('url-bar');
+
+function navigate() {
+    let url = urlBar.value;
+    if (!url) {
+        alert('Please enter a URL');
+        return;
+    }
+
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+        url = `https://${url}`;
+    }
+    
+    const proxyUrl = `http://localhost:3000/proxy?url=${encodeURIComponent(url)}`;
+    iframe.src = proxyUrl;
+}
+
+function goBack() {
+    iframe.contentWindow.history.back();
+}
+
+function goForward() {
+    iframe.contentWindow.history.forward();
+}
+
+function refreshPage() {
+    iframe.contentWindow.location.reload();
+}
+
+urlBar.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') navigate();
+});
+
+urlBar.value = 'https://example.com';
+navigate();
