091acc7e77e2c69bd8bf9cae725ebcacd12b1f7f
Andy64lolxd <andy64lolxd@gmail.com>
2025-03-28 17:08:13 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('searchButton').addEventListener('click', function() {
-    const query = document.getElementById('searchInput').value;
-    const searchUrl = `/search?q=${encodeURIComponent(query)}`;
-    document.getElementById('resultFrame').src = searchUrl;
-});
